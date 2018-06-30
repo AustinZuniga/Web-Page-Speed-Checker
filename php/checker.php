a21a0a73d885c5dd953f3c5da70ece7ac7e39f3d
AustinZuniga <earlaustinavilazuniga@gmail.com>
2018-06-30 22:20:15 +0800

    fixed bugs and added absolute paths

--- php/checker.php
+++ php/checker.php
@@ -1,21 +1,29 @@
-
 <?php
+// Earl Austin A. Zuniga
+// BSCS 4A BICOL UNIVERSITY
 
+// php script to get response time and speed in kb/s
+
+
+
+// file path
 $include = $_POST['file'];
 
 
 if (file_exists($include)) {
-    $starttime = microtime(); // default returns time as string
+    $starttime = microtime(); // returns time
     
-	ob_start(include($include)); // start output buffer
-
-	readfile($cachepage); // at some point read cache index file
+	ob_start();
+	include($include);
+	readfile($cachepage); // read cache
 	$page_response = microtime() - $starttime; // page response time
 	$speed = round((ob_get_length() / $page_response) / 1024, 3); //KB/s
-	
-	ob_end_clean();
 
+	$buffer = ob_get_clean(); // store buffer in variable
+	$buffer = ''; // empty buffer
+	ob_end_clean(); // clean
 
+	// the data
 	echo '{
 		"response" : "'.$page_response.'",
 		"speed" : "'.$speed.'"
@@ -24,6 +32,7 @@ if (file_exists($include)) {
 
 }
 else {
+	// if file does not exist
     echo 0;
 }
 
